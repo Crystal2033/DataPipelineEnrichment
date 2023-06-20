256d4cdee903001bf0e7ec3d3456adab53043c35
rahmatullintimur7777@gmail.com <rahmatullintimur7777@gmail.com>
2023-06-21 00:37:33 +0300

    some commit

--- src/main/java/ru/mai/lessons/rpks/impl/MongoDBClientEnricherImpl.java
+++ src/main/java/ru/mai/lessons/rpks/impl/MongoDBClientEnricherImpl.java
@@ -1,11 +1,15 @@
 package ru.mai.lessons.rpks.impl;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import com.typesafe.config.Config;
 import ru.mai.lessons.rpks.MongoDBClientEnricher;
 
 public class MongoDBClientEnricherImpl implements MongoDBClientEnricher {
-
     MongoDBClientEnricherImpl(Config config){
+        MongoClient mongoClient = MongoClients.create(config.getString("mongo.connectionString"));
+        MongoDatabase database = mongoClient.getDatabase("mongo.database");
 
     }
     @Override
